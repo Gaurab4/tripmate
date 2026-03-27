ae1e5ee9f71bb2b8d0290392e255af3f093a958a
gaurav <gauravthapa853@gmail.com>
2026-03-27 23:30:39 +0530

    Added Changes

--- tripmate-backend/tripmate/settings.py
+++ tripmate-backend/tripmate/settings.py
@@ -99,7 +99,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # ----------------------------
 # CORS CONFIG (FINAL FIX)
 # ----------------------------
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
